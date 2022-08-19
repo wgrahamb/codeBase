a24b1f28a295ad638a41b39788963adbf9a8a262
Graham Beech <graham@DESKTOP-8H1R25O.localdomain>
2022-08-19 16:06:56 -0500

    hellfire work -G

--- pythonFunctions/returnAzAndElevation.py
+++ pythonFunctions/returnAzAndElevation.py
@@ -8,3 +8,21 @@ def returnAzAndElevation(arr):
 def returnEl(y, x):
 	el = np.arctan2(y, x)
 	return el
+
+def returnAlphaAndBeta(arr):
+
+	alpha = None
+	beta = None
+
+	if (arr[2] == 0 and arr[0] == 0):
+		alpha = 0.0
+	else:
+		alpha = np.arctan2(arr[2], arr[0])
+
+	speed = np.linalg.norm(arr)
+	if speed == 0:
+		beta = 0.0
+	else:
+		beta = np.arcsin(arr[1] / speed)
+
+	return alpha, beta
